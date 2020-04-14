6e671b3b6e826fd911d191f7e74d2c223ee12dfd
Taku Ogawa <ogawtaku@gmail.com>
2020-04-14 21:08:27 +0900

    Add text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -8,17 +8,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Post App',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Post app'),
         ),
-        body: Center(
-          child: Text('Body contenets.'),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: ChangeForm(),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class ChangeForm extends StatefulWidget {
+  @override
+  ChangeFormState createState() => ChangeFormState();
+}
+
+class ChangeFormState extends State<ChangeForm> {
+  String text = "";
+
+  void handleText(String e) {
+    setState(() {
+      text = e;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(50.0),
+      child: Column(
+        children: <Widget>[
+          Text("$text",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w500)),
+          new TextField(
+            enabled: true,
+            maxLength: 10,
+            maxLengthEnforced: false,
+            style: TextStyle(color: Colors.red),
+            obscureText: false,
+            maxLines: 1,
+            onChanged: handleText,
+          ),
+        ],
       ),
     );
   }
